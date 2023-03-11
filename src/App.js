10436eafc6adccbf6da2a6f4def983ab3b5881d5
Austin Wang <yyaustin@bu.edu>
2023-03-10 23:45:42 -0500

    Finish exercise 2.6

---
+++ src/App.js
@@ -0,0 +1,45 @@
+const { useState } = require("react")
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {id: 1, name: "Arto Hellas"}
+  ])
+  const [newName, setNewName] = useState("")
+
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        debug: {newName}
+      </div>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (<li key={person.id}>{person.name}</li>))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
