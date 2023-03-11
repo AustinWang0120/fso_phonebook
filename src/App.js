1fcc86b2edd4e7e1175a90d49634ae9ec738d586
Austin Wang <yyaustin@bu.edu>
2023-03-11 00:06:11 -0500

    Finish exercise 2.8

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ const { useState } = require("react")
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {id: 1, name: "Arto Hellas"}
+    {id: 1, name: "Arto Hellas", number: "1234567"}
   ])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -16,14 +21,17 @@ const App = () => {
     if (persons.find((person) => person.name.toLowerCase() === newName.toLowerCase())) {
       window.alert(`${newName} is already added to phonebook`)
       setNewName("")
+      setNewNumber("")
       return
     }
     const personObject = {
       id: persons.length + 1,
       name: newName,
+      number: newNumber,
     }
     setPersons(persons.concat(personObject))
     setNewName("")
+    setNewNumber("")
   }
 
   return (
@@ -37,12 +45,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (<li key={person.id}>{person.name}</li>))}
+        {persons.map((person) => (<li key={person.id}>{person.name}: {person.number}</li>))}
       </ul>
     </div>
   )
